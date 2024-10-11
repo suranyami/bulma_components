c7d16be12557fbb894ea7c56413b3aca88707c4a
David Parry <david.parry@suranyami.com>
2024-10-11 13:37:45 +1100

    adds __using__ function for easy inclusion of library

--- lib/bulma_components.ex
+++ lib/bulma_components.ex
@@ -1,2 +1,28 @@
 defmodule BulmaComponents do
+  @moduledoc """
+  Imports all BulmaComponents.
+  """
+
+  def __using__(_opts) do
+    quote do
+      gettext_backend = Application.get_application(:bulma_components, :gettext_backend)
+      use Gettext, backend: gettext_backend
+
+      import BulmaComponents.BackLink
+      import BulmaComponents.Button
+      import BulmaComponents.Colors
+      import BulmaComponents.Flash
+      import BulmaComponents.Footer
+      import BulmaComponents.Form
+      import BulmaComponents.Header
+      import BulmaComponents.Hero
+      import BulmaComponents.Icon
+      import BulmaComponents.Input
+      import BulmaComponents.JsCommands
+      import BulmaComponents.List
+      import BulmaComponents.Modal
+      import BulmaComponents.Navbar
+      import BulmaComponents.Table
+    end
+  end
 end
