c98c19c4ee2df5ca3079cec28f424a689e8f39a6
David Parry <david.parry@suranyami.com>
2024-10-11 13:42:13 +1100

    add `gettext` config

--- storybook/config/config.exs
+++ storybook/config/config.exs
@@ -8,7 +8,8 @@
 import Config
 
 config :storybook,
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime],
+  gettext: StorybookWeb.Gettext
 
 # Configures the endpoint
 config :storybook, StorybookWeb.Endpoint,
